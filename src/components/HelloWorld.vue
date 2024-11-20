bce59bc5e0a9bc1243b12ed6240fe1615c8c2087
YinCheng <eason010630@gmail.com>
2024-11-20 16:22:45 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="gary">{{ msg }}</h1>
     <h3>
     </h3>
   </div>
--- src/components/ProjectMenu.vue
+++ src/components/ProjectMenu.vue
@@ -1,4 +1,12 @@
 <script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { ref } from 'vue';
+let id = 0;
+const project_list = ref([
+    {id: id++, name: "YINLA", img: "/favicon.ico", link: "https://github.com/YINLA-TEAM/YINLA", desc: "這是一隻Discord的BOT，不定時會更新 主要功能是檢視天氣、檢視地震(含推播功能)、文字翻譯、查詢中油油價(含推播功能)以及查詢中華職棒相關的功能"},
+    {id: id++, name: "discord-bot-rpg", img: "", link: "https://github.com/YINLA-TEAM/discord-bot-rpg", desc: "開發中..."},
+]);
 
 </script>
 
@@ -6,10 +14,31 @@
     <div>
         <h3>PROJECT</h3>
     </div>
+    <div>
+        <ul>
+            <li v-for="project in project_list" :key="project.id">
+                <img class="project_img" src=/favicon.ico>
+                <h3>{{ project.name }}</h3>
+                <p>{{ project.desc }}</p>
+                <a href="https://github.com/YINLA-TEAM/YINLA"><FontAwesomeIcon :icon="faLink" /> GitHub</a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <style scoped>
     h3 {
+        font-family: monospace;
         font-size: 1.2rem;
+    }
+
+    .project_img {
+        height: 64px;
+        float: right;
+        border-radius: 12px;
+    }
+
+    li {
+        
     }
 </style>

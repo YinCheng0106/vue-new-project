bf1725dffe000255f71d94172d5f2cf88268b3f3
YinCheng <eason010630@gmail.com>
2024-11-26 22:09:37 +0800

    update

--- src/components/ProjectMenu.vue
+++ src/components/ProjectMenu.vue
@@ -5,7 +5,7 @@ import { ref } from 'vue';
 
 let id = 0;
 const project_list = ref([
-    {id: id++, name: "YINLA", img: "/favicon.ico", link: "https://github.com/YINLA-TEAM/YINLA", desc: "這是一隻Discord的BOT，不定時會更新 主要功能是檢視天氣、檢視地震(含推播功能)、文字翻譯、查詢中油油價(含推播功能)以及查詢中華職棒相關的功能"},
+    {id: id++, name: "YINLA", img: "./favicon.ico", link: "https://github.com/YINLA-TEAM/YINLA", desc: "這是一隻Discord的BOT，不定時會更新 主要功能是檢視天氣、檢視地震(含推播功能)、文字翻譯、查詢中油油價(含推播功能)以及查詢中華職棒相關的功能"},
     {id: id++, name: "discord-bot-rpg", img: "", link: "https://github.com/YINLA-TEAM/discord-bot-rpg", desc: "開發中..."},
     {id: id++, name: "YINLA-TEAM.github.io", img: "", link: "https://github.com/YINLA-TEAM/YINLA-TEAM.github.io", desc: "YINLA 官網"},
 ]);
